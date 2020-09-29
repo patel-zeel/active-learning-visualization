d5e1c74df5d84fcdf66e6dfc5263c83bb0e18b8d
patel-zeel <59758528+patel-zeel@users.noreply.github.com>
2020-09-29 22:55:36 +0530

    Update remove_out.py

--- postprocessing/remove_out.py
+++ postprocessing/remove_out.py
@@ -1,8 +1,8 @@
 import re
 import os
-with open('active-visualization/notebook/main.html','r') as f:
+with open('notebook/main.html','r') as f:
 	data = f.read()
 data = re.sub('Out\[\d+\]:','',data)
 data = re.sub('<title>.*</title>','<title>Active Learning: A Visual Tour</title>',data)
-with open('active-visualization/index.html','w') as f:
+with open('index.html','w') as f:
         f.write(data)
